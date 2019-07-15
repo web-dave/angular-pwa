0767da8f4034177d7669405f3aee53f438c2c3a5
Dave <dave@webdave.de>
2019-07-15 23:10:11 +0200

    add methods

--- src/app/buddy.service.ts
+++ src/app/buddy.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,15 @@ export class BuddyService {
   url = 'http://localhost:3000/';
   constructor(private http: HttpClient) {}
 
-  getConferences() {
+  getConferences(): Observable<IConf[]> {
     return this.http.get<IConf[]>(this.url + 'conferences');
   }
-  getBuddies(id: number) {
+
+  addConferences(conf: IConf): Observable<IConf[]> {
+    return this.http.post<IConf[]>(this.url + 'conferences', conf);
+  }
+
+  getBuddies(id: number): Observable<IBuddy[]> {
     return this.http.get<IBuddy[]>(this.url + 'buddies').pipe(
       tap(b => console.log(b)),
       map(buddies =>
@@ -20,6 +26,12 @@ export class BuddyService {
       ),
       tap(b => console.log(b))
     );
+  }
+
+  addBuddies(buddy: IBuddy, id: number): Observable<IBuddy[]> {
+    return this.http
+      .post<IBuddy[]>(this.url + 'buddies', buddy)
+      .pipe(switchMap(() => this.getBuddies(id)));
   }
 }
 
